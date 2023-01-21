c6af430c2be5993540f4ad89cefdfb0388d9b6fc
Khun Kyaw Kham Hti <khunkyawkhamhti@Khuns-Air.local>
2023-01-21 22:12:38 +0630

    first

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView,
+} from "react-native";
 
-export default function App() {
+//https://reactnative.dev/movies.json
+const moviesURL = "https://reactnative.dev/movies.json";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [description, setDescription] = useState([]);
+
+  useEffect(() => {
+    fetch(moviesURL)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setData(json.movies);
+        setTitle(json.title);
+        setDescription(json.description);
+      })
+      .catch((error) => alert(error))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View style={{ paddingVertical: 15 }}>
+          <Text style={styles.title}>{title}</Text>
+          <View style={{ marginBottom: 15, borderBottomWidth: 1 }}></View>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }) => id}
+            renderItem={({ item }) => (
+              <View style={{ paddingBottom: 10 }}>
+                <Text style={styles.moviesText}>
+                  {item.id}, {item.title}, {item.releaseYear}
+                </Text>
+              </View>
+            )}
+          />
+          <Text style={styles.description}>{description}</Text>
+        </View>
+      )}
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  moviesText: {
+    fontSize: 26,
+    fontWeight: "200",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 18,
+    fontWeight: "200",
+    color: "#8EA7E9",
   },
 });
